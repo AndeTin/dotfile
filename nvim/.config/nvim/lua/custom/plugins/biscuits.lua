ca13788d35fba5a891c12f066065351b22608f5b
AndeTin <113510247+AndeTin@users.noreply.github.com>
2025-12-16 08:48:06 +0800

    feat: adding plugins to nvim
    biscuits: add comment at the end of brackets
    rederMarkdown: render markdown at normal mode
    toggle-number: toggling relevate number between normal and insert mode
    twilight: dimmer lines not around cursor

---
+++ nvim/.config/nvim/lua/custom/plugins/biscuits.lua
@@ -0,0 +1,19 @@
+return {
+  'code-biscuits/nvim-biscuits',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = {},
+  keys = {
+    {
+      '<leader>n',
+      function()
+        local nvim_biscuits = require 'nvim-biscuits'
+        nvim_biscuits.BufferAttach()
+        nvim_biscuits.toggle_biscuits()
+      end,
+      mode = 'n',
+      desc = 'Enable Biscuits',
+    },
+  },
+}
