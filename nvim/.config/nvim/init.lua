63cb3dc8d2ae9e9b1a54d3e6410fe243d98c7b46
AndeTin <113510247+AndeTin@users.noreply.github.com>
2026-08-11 13:40:11 +0800

    update: python lsp pyright

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -709,7 +709,7 @@ require('lazy').setup({
         clangd = {},
         phpactor = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
